75ba009285f91d8497fc186a2957a362224b9282
imtinannaqvi <imtinannadeem176@gmail.com>
2026-08-19 17:59:57 +0500

    Fix: case-sensitive Controllers import path for Linux deployment

--- server/routes/walletRoutes.js
+++ server/routes/walletRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { depositTest, getWallet } from '../controllers/dealController.js';
+import { depositTest, getWallet } from '../Controllers/dealController.js';
 import { protect } from '../middleware/auth.js';
 
 const router = express.Router();
